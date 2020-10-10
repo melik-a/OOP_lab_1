4c09c69fd2969f1b7b804d298381bf6a2c274920
milkymen <dzahelmelik@mail.ru>
2020-10-10 04:11:27 +0300

    checking for simplicity by root

--- Primes.java
+++ Primes.java
@@ -1,17 +1,23 @@
+import java.util.Date;
+
 public class Primes {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         for (int i = 2; i <= 100; i++){
             if (isPrime(i)){
                 System.out.println(i);
             }
         }
+        System.out.println("run time: " + (double) (System.currentTimeMillis() - startTime) / 1000 + " sec");
     }
 
     public static boolean isPrime(int chiselko) {
-        for (int i = 2;i<chiselko;i++){
-            if (chiselko % i == 0){
+        int i = 2;
+        while(i * i <= chiselko){
+            if(chiselko % i == 0){
                 return false;
             }
+            i++;
         }
         return true;
     }
